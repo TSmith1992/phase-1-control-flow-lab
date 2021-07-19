0ba35f555404ca8d2565890101ad499af790e1e6
Tarique Smith <tpsmith1992@gmail.com>
2021-07-19 11:12:47 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  if (number<=400){
+    return "This one is on me!"
+  } else if (number>2000 && number<2500){
+    return "I will gladly take your thirty bucks."
+  } else if (number>2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  if (city=='NYC'){
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
+} 
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+  case "generous":
+  return "Thank you so much.";
+  break;
+  case "not as generous":
+  return "Thank you.";
+  break;
+  default:
+  return "Bye."
+}
 }
